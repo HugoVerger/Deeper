2eae4a364f64dbea548cc4664cd4c2a2e22c6d9b
Hugo Verger <hverger@wanadoo.fr>
2016-09-20 23:39:20 +0200

    Ajout de plateformes et du manager de fin de partie

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager gm;
+	public GameObject player;
+
+	private enum gameStates {Building, Playing, BeatLevel, GameOver};
+	private gameStates gameState = gameStates.Playing;
+
+	public bool levelBeaten = false;
+	public GameObject beatLevelCanvas;
+	public AudioSource backgroundMusic;
+	public AudioClip beatLevelSFX;
+
+	void Start () {
+		if (gm == null) 
+			gm = gameObject.GetComponent<GameManager>();
+
+		if (player == null)
+			player = GameObject.FindWithTag("Player");
+
+		beatLevelCanvas.SetActive (false);
+	}
+
+	void Update () {
+		switch (gameState)
+		{
+			case gameStates.Building:
+				// nothing
+				break;
+			case gameStates.Playing:
+				if (levelBeaten) {
+					// update gameState
+					gameState = gameStates.BeatLevel;
+
+					// hide the player so game doesn't continue playing
+					player.SetActive(false);
+
+					// switch which GUI is showing			
+					beatLevelCanvas.SetActive (true);
+				}
+				break;
+			case gameStates.BeatLevel:
+				backgroundMusic.volume -= 0.01f;
+				if (backgroundMusic.volume<=0.0f) {
+					AudioSource.PlayClipAtPoint (beatLevelSFX,gameObject.transform.position,0.5f);
+					
+					gameState = gameStates.GameOver;
+				}
+				break;
+			case gameStates.GameOver:
+				// nothing
+				break;
+		}
+	}
+
+	public void beatLevel() {
+		levelBeaten = true;
+	}
+}
---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+	
+	public string LevelToLoad;
+	
+	public void loadLevel() {
+		//Load the level from LevelToLoad
+		SceneManager.LoadScene(LevelToLoad, LoadSceneMode.Single);
+	}
+}
