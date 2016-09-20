d31c1a9aa515e13197b1511bf765c21a2b2a1f57
Hugo Verger <hverger@wanadoo.fr>
2016-09-21 01:42:31 +0200

    Controle manuel de la boule

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	public float force = 50f;
+
+	void Update () {
+		float horizontalMove = Input.GetAxis ("Horizontal");
+		GetComponent<Rigidbody2D> ().AddForce (new Vector2 (horizontalMove, 0) * force * Time.deltaTime);
+	}
+}
