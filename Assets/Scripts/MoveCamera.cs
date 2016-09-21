1237f2d026b45f50b7e3dd5f47708d81a43c2513
Hugo Verger <hverger@wanadoo.fr>
2016-09-21 10:40:57 +0200

    La caméra bouge !

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamera : MonoBehaviour {
+
+	public float speed = 0.150f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (new Vector3 (0, -speed, 0));
+	}
+}
