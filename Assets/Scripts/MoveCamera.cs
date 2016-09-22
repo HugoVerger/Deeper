f211a4fa4c0e4aae28c5de96d701458b7fddb246
Hugo Verger <hverger@wanadoo.fr>
2016-09-22 07:59:44 +0200

    design de 2 niveaux

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -1,12 +1,28 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 using System.Collections;
 
 public class MoveCamera : MonoBehaviour {
 
-	public float speed = 0.150f;
+	public float speed = 0.050f;
+	public GameObject ball;
+	private bool cameraStarted = false;
+
+	void Start() {
+		if (ball == null) {
+			ball = GameObject.Find ("Ball");
+		}
+		cameraStarted = false;
+		ball.GetComponent<Rigidbody2D> ().isKinematic = true;
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (new Vector3 (0, -speed, 0));
+		if (!cameraStarted && (CrossPlatformInputManager.GetAxis ("Horizontal") > 0 || CrossPlatformInputManager.GetAxis ("Horizontal") < 0)) {
+			cameraStarted = true;
+			ball.GetComponent<Rigidbody2D> ().isKinematic = false;
+		} else if (cameraStarted) {
+			transform.Translate (new Vector3 (0, -speed, 0));
+		}
 	}
 }
